49f05eaabd9dfed1b5bf54673a54d146c1a98fac
malcolmszx <malcolmszx@gmail.com>
2018-11-03 12:43:12 +0800

    非对称算法不同方式使用

--- springboot/src/main/java/com/kingdee/abc/util/RSAUtil.java
+++ springboot/src/main/java/com/kingdee/abc/util/RSAUtil.java
@@ -5,9 +5,14 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +20,13 @@ import javax.crypto.Cipher;
 
 import org.apache.commons.codec.binary.Base64;
 
+import lombok.extern.slf4j.Slf4j;
 
+/*
+ * autor:malcolmszx
+ * https://blog.csdn.net/u013565368/article/details/53081195
+ */
+@Slf4j
 public class RSAUtil {
 	
 	/** 指定加密算法为RSA */
@@ -29,10 +40,12 @@ public class RSAUtil {
     /** 指定私钥存放文件 */
     private static String PRIVATE_KEY_FILE = "PrivateKey";
 
+    
+    /**************************************** 方式一 **********************************************/
     /**
      * 生成密钥对
      */
-    private static void generateKeyPair() throws Exception {
+    private static void generateKeyPairFile() throws Exception {
         /** RSA算法要求有一个可信任的随机数源 */
         SecureRandom sr = new SecureRandom();
         /** 为RSA算法创建一个KeyPairGenerator对象 */
@@ -46,10 +59,8 @@ public class RSAUtil {
         /** 得到私钥 */
         Key privateKey = kp.getPrivate();
         /** 用对象流将生成的密钥写入文件 */
-        ObjectOutputStream oos1 = new ObjectOutputStream(new FileOutputStream(
-                PUBLIC_KEY_FILE));
-        ObjectOutputStream oos2 = new ObjectOutputStream(new FileOutputStream(
-                PRIVATE_KEY_FILE));
+        ObjectOutputStream oos1 = new ObjectOutputStream(new FileOutputStream(PUBLIC_KEY_FILE));
+        ObjectOutputStream oos2 = new ObjectOutputStream(new FileOutputStream(PRIVATE_KEY_FILE));
         oos1.writeObject(publicKey);
         oos2.writeObject(privateKey);
         /** 清空缓存，关闭文件输出流 */
@@ -92,9 +103,9 @@ public class RSAUtil {
         return new String(b);
     }
     
-    
-    //以base64编码密钥
-    public static Map<String ,String> generateKeyPair1() throws Exception{
+    /**************************************** 方式二 **********************************************/
+    /**以base64编码密钥 */
+    public static Map<String ,String> generateKeyPair() throws Exception{
         SecureRandom sr = new SecureRandom();
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
         kpg.initialize(KEYSIZE, sr);
@@ -110,20 +121,54 @@ public class RSAUtil {
         map.put("privateKey", prStr);
         return map;
     }
+    
+    /**从base64编码的公钥恢复私钥 */
+    public static PublicKey getPulbickey(String key_base64) throws Exception{
+    	byte[] pb = new Base64().decode(key_base64);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(pb);
+        KeyFactory  keyfactory = KeyFactory.getInstance("RSA");
+        return keyfactory.generatePublic(keySpec);
+    }
+    /**从base64编码的私钥恢复私钥 */
+    public static PrivateKey getPrivatekey(String key_base64) throws Exception{
+        byte[] pb =  new Base64().decode(key_base64);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pb);
+        KeyFactory  keyfactory = KeyFactory.getInstance("RSA");
+        return keyfactory.generatePrivate(keySpec);
+    }
+    
+    /** 执行加密操作 */
+    public static byte[] encrypt(Key key,byte[] source) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] ciphertext = cipher.doFinal(source);
+        return ciphertext;
+    }
+    /** 执行解密操作 */
+    public static byte[] decrypt(Key key,byte[] ciphertext) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] source = cipher.doFinal(ciphertext);
+        return source;
+    }
 
-
+    
+  /*  
     public static void main(String[] args) throws Exception {
-    	
-    	generateKeyPair1();
-    	
-        generateKeyPair();//生成密钥对
+    	generateKeyPairFile();//生成密钥对
         String source = "Hello World, Hell0 , Wang Yuan ";// 要加密的字符串
         byte[] cryptograph = encrypt(source);// 生成的密文
-
         //可以将密文进行base64编码进行传输
         System.out.println(new String(Base64.decodeBase64(cryptograph)));
         String target = decrypt(cryptograph);// 解密密文
         System.out.println(target);
+    }*/
+    
+    public static void main(String[] args) throws Exception {
+    	Map<String ,String> keyPairMap = generateKeyPair();
+    	log.info("【keyPairMap】============== keyPairMap: {}", keyPairMap);
+    	PublicKey publicKey = getPulbickey(keyPairMap.get("publicKey"));
+    	PrivateKey privateKey = getPrivatekey(keyPairMap.get("privateKey"));
     }
 
 	
